afe466020ee5ff938d677597ee9d4bfbc94c304c
r.oleg.official <r.oleg.official@gmail.com>
2023-01-26 17:32:57 +0300

    Done seminar02/task03_v2.java.

---
+++ src/seminar02/task03_v2.java
@@ -0,0 +1,46 @@
+/*
+
+ * Classname: task03_v2
+ *
+ * Version: 1.
+ *
+ * Date: 2023-01-26.
+ *
+ * Copyright notice: by Oleg Razumov.
+ */
+
+package seminar02;
+
+public class task03_v2 {
+
+    public static String findSimpleNumber(int min, int max) {
+        StringBuilder sb = new StringBuilder();
+        int counter;
+        int number = min;
+
+        while (number <= max) {
+             counter = 0;
+
+            for (int i = min; i <= number; i++) {
+                if (number % i == 0) counter++;
+            }
+
+            if (counter < 2) sb.append(number + ", ");
+            number++;
+        }
+        return sb.substring(0, sb.length() - 2);
+    }
+
+    public static void main(String[] args) {
+        /*
+        Task3
+        Напишите программу, которая выводит на консоль простые числа в промежутке от [2, 100].
+        Используйте для решения этой задачи оператор "%" (остаток от деления) и циклы.
+         */
+
+        int number_min = 2;
+        int number_max = 100;
+
+        System.out.println(findSimpleNumber(number_min, number_max));
+    }
+}
