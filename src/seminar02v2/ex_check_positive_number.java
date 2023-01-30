b1d32d1cb7f17de6e3dce9a1ef6f385f9f25fdff
r.oleg.official <r.oleg.official@gmail.com>
2023-01-30 11:11:00 +0300

    Add seminar02v2/ex_check_positive_number.java. Example use regular. In progress.

---
+++ src/seminar02v2/ex_check_positive_number.java
@@ -0,0 +1,14 @@
+package seminar02v2;
+
+import java.util.Scanner;
+
+public class ex_check_positive_number {
+    public static void main(String[] args) {
+        int size = 0;
+        System.out.print("Enter a even positive number: ");
+        Scanner scn = new Scanner(System.in);
+        String regex = "^[1-9]\\d*$";
+        //while (!input.matches(regex) || Integer.parseInt(input) % 2 != 0)
+        System.out.println(size);
+    }
+}
