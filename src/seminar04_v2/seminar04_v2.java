20a647885526389c1224fb239bc6cccdaf723927
r.oleg.official <r.oleg.official@gmail.com>
2023-01-31 18:38:18 +0300

    Edit seminar04_v2/seminar04_v2.java.

--- src/seminar04_v2/seminar04_v2.java
+++ src/seminar04_v2/seminar04_v2.java
@@ -17,9 +17,9 @@ import java.util.Deque;
 
 public class seminar04_v2 {
     public static void main(String[] args) {
-        //System.out.println("--Task 01.");
-        //task01();
-        //System.out.println();
+        System.out.println("--Task 01.");
+        task01();
+        System.out.println();
         System.out.println("--Task 02.");
         task02();
         System.out.println();
@@ -28,7 +28,7 @@ public class seminar04_v2 {
     public static void task01() {
         // Дан Deque состоящий из последовательности цифр.
         // Необходимо проверить, что последовательность цифр является палиндромом.
-        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1,2,3,4,5,6));
+        Deque<Integer> deque = new ArrayDeque<>(Arrays.asList(1, 2, 3, 4, 5, 6));
 
         System.out.print(deque);
         if (checkOn(deque) == true) System.out.println(" - it's palindrome.");
@@ -49,16 +49,19 @@ public class seminar04_v2 {
         }
         return true;
     }
+
     public static void task02() {
-    //Даны два Deque представляющие два неотрицательных целых числа. Цифры хранятся в обратном порядке,
-    // и каждый из их узлов содержит одну цифру.
-    // Сложите два числа и верните сумму в виде связанного списка.
+        //Даны два Deque представляющие два неотрицательных целых числа. Цифры хранятся в обратном порядке,
+        // и каждый из их узлов содержит одну цифру.
+        // Сложите два числа и верните сумму в виде связанного списка.
+
 //        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(1,2,3));
 //        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(5,4,7));
 //         result [6,6,0,1]
 
-        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(3,2,9));
-        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(7,4,5));
+        Deque<Integer> d1 = new ArrayDeque<>(Arrays.asList(3, 2, 9));
+        Deque<Integer> d2 = new ArrayDeque<>(Arrays.asList(7, 4, 5));
+//         result [1,0,7,4]
 
         System.out.printf("%s\n%s\n", d1, d2);
         Deque<Integer> d3 = new ArrayDeque<>();
@@ -67,27 +70,29 @@ public class seminar04_v2 {
         if (d1.size() > d2.size()) size = d1.size();
         else size = d2.size();
 
-        int sum = 0;
-        for (int i = 0; i < size; i++) {
-            int a = d1.pollLast();
-            int b = d2.pollLast();
-            System.out.printf("%d, %d.\n", a, b);
-            sum += a + b;
-            if (sum < 9) {
-                d3.addLast(sum);
-            } else {
-                while (sum > 9) {
-                    d3.addLast(sum % 10);
-                    sum /= 10;
-//                    if (d1.size() > 0 && (d2.size() > 0)) {
-//                        sum += d1.pollLast() + d2.pollLast() + sum;
-//                    }
-                }
-                d3.addLast(sum);
-            }
+        int sum, tmp = 0;
+        while (size > 0) {
+            int a = d1.pollFirst();
+            int b = d2.pollFirst();
             //System.out.printf("%d, %d.\n", a, b);
+            sum = a + b + tmp;
+            if (sum < 9) {
+                d3.add(sum);
+                tmp = 0;
+                //sum = 0;
+            }
+            if (sum > 9) {
+                d3.add(sum % 10);
+                tmp = sum /= 10;
+                //sum /= 10;
+            }
+            //d3.addFirst(sum);
+            //sum = 0;
+            size--;
+            if (size == 0) d3.addFirst(sum);
         }
+
         System.out.println(d3);
     }
-
 }
+
